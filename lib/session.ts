dcbfaa7e751b223b215ad0d3fa6f950d51ff4913
ad <adhiraj2004@icloud.com>
2023-07-13 04:22:12 +0530

    commit

--- lib/session.ts
+++ lib/session.ts
@@ -4,7 +4,9 @@ import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 import jsonwebtoken from 'jsonwebtoken'
 import { JWT } from 'next-auth/jwt';
-
+import { getUser } from "./actions";
+import { SessionInterface, UserProfile } from "@/common.types";
+import { createUser } from "./actions";
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
@@ -27,13 +29,36 @@ export const authOptions: NextAuthOptions = {
     },
     callbacks: {
         async session({ session }){
-            return session;
+            const email = session?.user?.email as string;
+
+            try {
+                const data = await getUser(email) as {user? :
+                UserProfile}
+
+                const newSession = {
+                    ...session,
+                    user: { 
+                        ...session.user,
+                        ...data?.user
+                    }
+                }
+                return newSession
+            } catch (error) {
+                console.log('Error retrieving user data', error);
+                return session;
+            }
         },
         async signIn({ user }: {user: AdapterUser | User}){
            try {
-              //get user if they exist
+              const userExists = await getUser(user?.email as
+                string ) as { user?:UserProfile }
               
-              //if they dont exist , create them
+              if(!userExists.user){
+               await createUser(
+                user.name as string , 
+                user.email as string , 
+                user.image as string);
+              }
 
               return true;
            } catch (error: any) {
@@ -43,3 +68,8 @@ export const authOptions: NextAuthOptions = {
         }
     }
 }
+
+export async function getCurrentUser(): Promise<SessionInterface> {
+    const session = (await getServerSession(authOptions)) as SessionInterface;
+    return session;
+  }
